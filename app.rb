3da0be7ef3c999309a3ca128a4257b89b419ee76
Muhammed Ahmad <99442871+Muhammed-L-Ahmad@users.noreply.github.com>
2025-05-05 17:20:59 +0000

    several changes integrated

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
+  erb(:homepage)
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
@@ -27,9 +28,28 @@ erb(:rock)
 end
 
 get ("/paper") do
+  moves = ["paper", "scissors", "rock"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "paper"
+    @outcome = "tied"
+  elsif @comp_move == "scissors"
+    @outcome = "lost"
+  else
+    @outcome = "won"
 
 end
 
 get ("/scissors") do
+  moves = ["scissors", "rock", "paper"]
 
+  @comp_move = moves.sample
+
+  if @comp_move == "scissors"
+    @outcome = "tied"
+  elsif @comp_move == "rock"
+    @outcome = "lost"
+  else
+    @outcome = "won"
 end
